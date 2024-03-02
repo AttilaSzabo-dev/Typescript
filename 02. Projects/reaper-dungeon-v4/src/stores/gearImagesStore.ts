ea8513d93a2056b638f0a09094e57c29fcddcf96
Attila Szabo <attila.szabo.vrc@gmail.com>
2024-03-02 12:09:26 +0100

    gear image store

---
+++ 02. Projects/reaper-dungeon-v4/src/stores/gearImagesStore.ts
@@ -0,0 +1,456 @@
+import basic_belt_1 from "../assets/images/gear/belt/basic_belt_1.png";
+import basic_belt_2 from "../assets/images/gear/belt/basic_belt_2.png";
+import advanced_belt_1 from "../assets/images/gear/belt/advanced_belt_1.png";
+import advanced_belt_2 from "../assets/images/gear/belt/advanced_belt_2.png";
+import rare_belt_1 from "../assets/images/gear/belt/rare_belt_1.png";
+import rare_belt_2 from "../assets/images/gear/belt/rare_belt_2.png";
+import magic_belt_1 from "../assets/images/gear/belt/magic_belt_1.png";
+import magic_belt_2 from "../assets/images/gear/belt/magic_belt_2.png";
+import set_belt_1 from "../assets/images/gear/belt/set_belt_1.png";
+import set_belt_2 from "../assets/images/gear/belt/set_belt_2.png";
+
+import basic_body_1 from "../assets/images/gear/body/basic_body_1.png";
+import basic_body_2 from "../assets/images/gear/body/basic_body_2.png";
+import advanced_body_1 from "../assets/images/gear/body/advanced_body_1.png";
+import advanced_body_2 from "../assets/images/gear/body/advanced_body_2.png";
+import rare_body_1 from "../assets/images/gear/body/rare_body_1.png";
+import rare_body_2 from "../assets/images/gear/body/rare_body_2.png";
+import set_body_1 from "../assets/images/gear/body/set_body_1.png";
+import set_body_2 from "../assets/images/gear/body/set_body_2.png";
+import no_body from "../assets/images/gear/body/no_body.png";
+
+import basic_feet_1 from "../assets/images/gear/feet/basic_feet_1.png";
+import basic_feet_2 from "../assets/images/gear/feet/basic_feet_2.png";
+import advanced_feet_1 from "../assets/images/gear/feet/advanced_feet_1.png";
+import advanced_feet_2 from "../assets/images/gear/feet/advanced_feet_2.png";
+import rare_feet_1 from "../assets/images/gear/feet/rare_feet_1.png";
+import rare_feet_2 from "../assets/images/gear/feet/rare_feet_2.png";
+import magic_feet_1 from "../assets/images/gear/feet/magic_feet_1.png";
+import set_feet_1 from "../assets/images/gear/feet/set_feet_1.png";
+import set_feet_2 from "../assets/images/gear/feet/set_feet_2.png";
+import no_feet from "../assets/images/gear/feet/no_feet.png";
+
+import basic_hand_1 from "../assets/images/gear/hand/basic_hand_1.png";
+import basic_hand_2 from "../assets/images/gear/hand/basic_hand_2.png";
+import advanced_hand_1 from "../assets/images/gear/hand/advanced_hand_1.png";
+import advanced_hand_2 from "../assets/images/gear/hand/advanced_hand_2.png";
+import rare_hand_1 from "../assets/images/gear/hand/rare_hand_1.png";
+import rare_hand_2 from "../assets/images/gear/hand/rare_hand_2.png";
+import magic_hand_1 from "../assets/images/gear/hand/magic_hand_1.png";
+import magic_hand_2 from "../assets/images/gear/hand/magic_hand_2.png";
+import set_hand_1 from "../assets/images/gear/hand/set_hand_1.png";
+import set_hand_2 from "../assets/images/gear/hand/set_hand_2.png";
+import no_hand from "../assets/images/gear/hand/no_hand.png";
+
+import basic_head_1 from "../assets/images/gear/head/basic_head_1.png";
+import basic_head_2 from "../assets/images/gear/head/basic_head_2.png";
+import advanced_head_1 from "../assets/images/gear/head/advanced_head_1.png";
+import advanced_head_2 from "../assets/images/gear/head/advanced_head_2.png";
+import rare_head_1 from "../assets/images/gear/head/rare_head_1.png";
+import rare_head_2 from "../assets/images/gear/head/rare_head_2.png";
+import magic_head_1 from "../assets/images/gear/head/magic_head_1.png";
+import set_head_1 from "../assets/images/gear/head/set_head_1.png";
+import set_head_2 from "../assets/images/gear/head/set_head_2.png";
+import no_head from "../assets/images/gear/head/no_head.png";
+
+import basic_leg_1 from "../assets/images/gear/leg/basic_leg_1.png";
+import basic_leg_2 from "../assets/images/gear/leg/basic_leg_2.png";
+import advanced_leg_1 from "../assets/images/gear/leg/advanced_leg_1.png";
+import advanced_leg_2 from "../assets/images/gear/leg/advanced_leg_2.png";
+import rare_leg_1 from "../assets/images/gear/leg/rare_leg_1.png";
+import rare_leg_2 from "../assets/images/gear/leg/rare_leg_2.png";
+import magic_leg_1 from "../assets/images/gear/leg/magic_leg_1.png";
+import set_leg_1 from "../assets/images/gear/leg/set_leg_1.png";
+import set_leg_2 from "../assets/images/gear/leg/set_leg_2.png";
+import no_leg from "../assets/images/gear/leg/no_leg.png";
+
+import basic_shield_1 from "../assets/images/gear/shield/basic_shield_1.png";
+import basic_shield_2 from "../assets/images/gear/shield/basic_shield_2.png";
+import advanced_shield_1 from "../assets/images/gear/shield/advanced_shield_1.png";
+import advanced_shield_2 from "../assets/images/gear/shield/advanced_shield_2.png";
+import rare_shield_1 from "../assets/images/gear/shield/rare_shield_1.png";
+import rare_shield_2 from "../assets/images/gear/shield/rare_shield_2.png";
+import legendary_shield_1 from "../assets/images/gear/shield/legendary_shield_1.png";
+import legendary_shield_2 from "../assets/images/gear/shield/legendary_shield_2.png";
+import magic_shield_1 from "../assets/images/gear/shield/magic_shield_1.png";
+import magic_shield_2 from "../assets/images/gear/shield/magic_shield_2.png";
+import set_shield_1 from "../assets/images/gear/shield/set_shield_1.png";
+import set_shield_2 from "../assets/images/gear/shield/set_shield_2.png";
+
+import basic_sword_1 from "../assets/images/gear/sword/basic_sword_1.png";
+import basic_sword_2 from "../assets/images/gear/sword/basic_sword_2.png";
+import advanced_sword_1 from "../assets/images/gear/sword/advanced_sword_1.png";
+import advanced_sword_2 from "../assets/images/gear/sword/advanced_sword_2.png";
+import rare_sword_1 from "../assets/images/gear/sword/rare_sword_1.png";
+import rare_sword_2 from "../assets/images/gear/sword/rare_sword_2.png";
+import legendary_sword_1 from "../assets/images/gear/sword/legendary_sword_1.png";
+import legendary_sword_2 from "../assets/images/gear/sword/legendary_sword_2.png";
+import magic_sword_1 from "../assets/images/gear/sword/magic_sword_1.png";
+import magic_sword_2 from "../assets/images/gear/sword/magic_sword_2.png";
+import set_sword_1 from "../assets/images/gear/sword/set_sword_1.png";
+import set_sword_2 from "../assets/images/gear/sword/set_sword_2.png";
+import no_weapon from "../assets/images/gear/sword/no_weapon.png";
+
+export const belt = [
+  {
+    id: "b_be_1",
+    image: basic_belt_1,
+  },
+  {
+    id: "b_be_2",
+    image: basic_belt_2,
+  },
+  {
+    id: "a_be_1",
+    image: advanced_belt_1,
+  },
+  {
+    id: "a_be_2",
+    image: advanced_belt_2,
+  },
+  {
+    id: "r_be_1",
+    image: rare_belt_1,
+  },
+  {
+    id: "r_be_2",
+    image: rare_belt_2,
+  },
+  {
+    id: "m_be_1",
+    image: magic_belt_1,
+  },
+  {
+    id: "m_be_2",
+    image: magic_belt_2,
+  },
+  {
+    id: "s_be_1",
+    image: set_belt_1,
+  },
+  {
+    id: "s_be_2",
+    image: set_belt_2,
+  },
+];
+
+export const body = [
+  {
+    id: "b_bo_1",
+    image: basic_body_1,
+  },
+  {
+    id: "b_bo_2",
+    image: basic_body_2,
+  },
+  {
+    id: "a_bo_1",
+    image: advanced_body_1,
+  },
+  {
+    id: "a_bo_2",
+    image: advanced_body_2,
+  },
+  {
+    id: "r_bo_1",
+    image: rare_body_1,
+  },
+  {
+    id: "r_bo_2",
+    image: rare_body_2,
+  },
+  {
+    id: "s_bo_1",
+    image: set_body_1,
+  },
+  {
+    id: "s_bo_2",
+    image: set_body_2,
+  },
+  {
+    id: "no_bo",
+    image: no_body,
+  },
+];
+
+export const feet = [
+  {
+    id: "b_fe_1",
+    image: basic_feet_1,
+  },
+  {
+    id: "b_fe_2",
+    image: basic_feet_2,
+  },
+  {
+    id: "a_fe_1",
+    image: advanced_feet_1,
+  },
+  {
+    id: "a_fe_2",
+    image: advanced_feet_2,
+  },
+  {
+    id: "r_fe_1",
+    image: rare_feet_1,
+  },
+  {
+    id: "r_fe_2",
+    image: rare_feet_2,
+  },
+  {
+    id: "m_fe_1",
+    image: magic_feet_1,
+  },
+  {
+    id: "s_fe_1",
+    image: set_feet_1,
+  },
+  {
+    id: "s_fe_2",
+    image: set_feet_2,
+  },
+  {
+    id: "no_fe",
+    image: no_feet,
+  },
+];
+
+export const hand = [
+  {
+    id: "b_ha_1",
+    image: basic_hand_1,
+  },
+  {
+    id: "b_ha_2",
+    image: basic_hand_2,
+  },
+  {
+    id: "a_ha_1",
+    image: advanced_hand_1,
+  },
+  {
+    id: "a_ha_2",
+    image: advanced_hand_2,
+  },
+  {
+    id: "r_ha_1",
+    image: rare_hand_1,
+  },
+  {
+    id: "r_ha_2",
+    image: rare_hand_2,
+  },
+  {
+    id: "m_ha_1",
+    image: magic_hand_1,
+  },
+  {
+    id: "m_ha_2",
+    image: magic_hand_2,
+  },
+  {
+    id: "s_ha_1",
+    image: set_hand_1,
+  },
+  {
+    id: "s_ha_2",
+    image: set_hand_2,
+  },
+  {
+    id: "no_ha",
+    image: no_hand,
+  },
+];
+
+export const head = [
+  {
+    id: "b_he_1",
+    image: basic_head_1,
+  },
+  {
+    id: "b_he_2",
+    image: basic_head_2,
+  },
+  {
+    id: "a_he_1",
+    image: advanced_head_1,
+  },
+  {
+    id: "a_he_2",
+    image: advanced_head_2,
+  },
+  {
+    id: "r_he_1",
+    image: rare_head_1,
+  },
+  {
+    id: "r_he_2",
+    image: rare_head_2,
+  },
+  {
+    id: "m_he_1",
+    image: magic_head_1,
+  },
+  {
+    id: "s_he_1",
+    image: set_head_1,
+  },
+  {
+    id: "s_he_2",
+    image: set_head_2,
+  },
+  {
+    id: "no_he",
+    image: no_head,
+  },
+];
+
+export const leg = [
+  {
+    id: "b_le_1",
+    image: basic_leg_1,
+  },
+  {
+    id: "b_le_2",
+    image: basic_leg_2,
+  },
+  {
+    id: "a_le_1",
+    image: advanced_leg_1,
+  },
+  {
+    id: "a_le_2",
+    image: advanced_leg_2,
+  },
+  {
+    id: "r_le_1",
+    image: rare_leg_1,
+  },
+  {
+    id: "r_le_2",
+    image: rare_leg_2,
+  },
+  {
+    id: "m_le_1",
+    image: magic_leg_1,
+  },
+  {
+    id: "s_le_1",
+    image: set_leg_1,
+  },
+  {
+    id: "s_le_2",
+    image: set_leg_2,
+  },
+  {
+    id: "no_le",
+    image: no_leg,
+  },
+];
+
+export const shield = [
+  {
+    id: "b_sh_1",
+    image: basic_shield_1,
+  },
+  {
+    id: "b_sh_2",
+    image: basic_shield_2,
+  },
+  {
+    id: "a_sh_1",
+    image: advanced_shield_1,
+  },
+  {
+    id: "a_sh_2",
+    image: advanced_shield_2,
+  },
+  {
+    id: "r_sh_1",
+    image: rare_shield_1,
+  },
+  {
+    id: "r_sh_2",
+    image: rare_shield_2,
+  },
+  {
+    id: "l_sh_1",
+    image: legendary_shield_1,
+  },
+  {
+    id: "l_sh_2",
+    image: legendary_shield_2,
+  },
+  {
+    id: "m_sh_1",
+    image: magic_shield_1,
+  },
+  {
+    id: "m_sh_2",
+    image: magic_shield_2,
+  },
+  {
+    id: "s_sh_1",
+    image: set_shield_1,
+  },
+  {
+    id: "s_sh_2",
+    image: set_shield_2,
+  },
+];
+
+export const sword = [
+  {
+    id: "b_sw_1",
+    image: basic_sword_1,
+  },
+  {
+    id: "b_sw_2",
+    image: basic_sword_2,
+  },
+  {
+    id: "a_sw_1",
+    image: advanced_sword_1,
+  },
+  {
+    id: "a_sw_2",
+    image: advanced_sword_2,
+  },
+  {
+    id: "r_sw_1",
+    image: rare_sword_1,
+  },
+  {
+    id: "r_sw_2",
+    image: rare_sword_2,
+  },
+  {
+    id: "l_sw_1",
+    image: legendary_sword_1,
+  },
+  {
+    id: "l_sw_2",
+    image: legendary_sword_2,
+  },
+  {
+    id: "m_sw_1",
+    image: magic_sword_1,
+  },
+  {
+    id: "m_sw_2",
+    image: magic_sword_2,
+  },
+  {
+    id: "s_sw_1",
+    image: set_sword_1,
+  },
+  {
+    id: "s_sw_2",
+    image: set_sword_2,
+  },
+  {
+    id: "no_sw",
+    image: no_weapon,
+  },
+];
